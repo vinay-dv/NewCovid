bd39df24b7206f92ebabe2c58d7d7e8d0965b50b
vinay-dv <40320340+vinay-dv@users.noreply.github.com>
2020-04-09 18:53:23 +0530

    Update DetailActivity.java

--- app/src/main/java/com/vinay/covid19/DetailActivity.java
+++ app/src/main/java/com/vinay/covid19/DetailActivity.java
@@ -3,20 +3,16 @@ package com.vinay.covid19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.vinay.covid19.MainActivity.EXTRA_mCountryName;
-import static com.vinay.covid19.MainActivity.EXTRA_mImageResource;
 import static com.vinay.covid19.MainActivity.EXTRA_mactive;
 import static com.vinay.covid19.MainActivity.EXTRA_mcases;
 import static com.vinay.covid19.MainActivity.EXTRA_mcritical;
@@ -25,11 +21,8 @@ import static com.vinay.covid19.MainActivity.EXTRA_mnewCases;
 import static com.vinay.covid19.MainActivity.EXTRA_mnewDeaths;
 import static com.vinay.covid19.MainActivity.EXTRA_mrecovered;
 import static com.vinay.covid19.MainActivity.EXTRA_mtime;
-import static com.vinay.covid19.MainActivity.country_list;
-
 
 public class DetailActivity extends AppCompatActivity {
-
     public ImageView dImageView;
     public TextView dCountryName;
     public TextView dCases;
@@ -40,6 +33,7 @@ public class DetailActivity extends AppCompatActivity {
     public TextView dCriticalCases;
     public TextView dNewDeaths;
     public TextView dDateTime;
+  //  public EditText deditText;
     public Bitmap iImageResource;
     public String iCountryName;
     public String iCases;
@@ -50,14 +44,17 @@ public class DetailActivity extends AppCompatActivity {
     public String iCriticalCases;
     public String iNewDeaths;
     public String iDateTime;
-    public int position;
-    private AssetManager assetManager;
+
+    @Override
+    public void finish(){
+        super.finish();
+        overridePendingTransition(R.anim.left,R.anim.right);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
         dImageView     = findViewById(R.id.dcountryflagimageView);
         dCountryName   = findViewById(R.id.dcountryName);
         dCases         = findViewById(R.id.ddisplayCase);
@@ -68,10 +65,12 @@ public class DetailActivity extends AppCompatActivity {
         dCriticalCases = findViewById(R.id.ddisplayCriticalCases);
         dNewDeaths     = findViewById(R.id.ddsiplayNewDewaths);
         dDateTime      = findViewById(R.id.displayDateTime);
+    //    deditText      = findViewById(R.id.displayiSupport);
 
         Intent intent = getIntent();
        // Country_item country_item = intent.getParcelableExtra("country_item");
-        iImageResource = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("BitmapImage"),0,getIntent().getByteArrayExtra("BitmapImage").length);
+        iImageResource = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("BitmapImage")
+                ,0,getIntent().getByteArrayExtra("BitmapImage").length);
         iCountryName    = intent.getStringExtra(EXTRA_mCountryName);
         iCases          = intent.getStringExtra(EXTRA_mcases);
         iDeaths         = intent.getStringExtra(EXTRA_mdeaths);
@@ -79,9 +78,8 @@ public class DetailActivity extends AppCompatActivity {
         iNewCases       = intent.getStringExtra(EXTRA_mnewCases);
         iActiveCases    = intent.getStringExtra(EXTRA_mactive);
         iCriticalCases  = intent.getStringExtra(EXTRA_mcritical);
-        iNewDeaths = intent.getStringExtra(EXTRA_mnewDeaths);
-        iDateTime = intent.getStringExtra(EXTRA_mtime);
-
+        iNewDeaths      = intent.getStringExtra(EXTRA_mnewDeaths);
+        iDateTime       = intent.getStringExtra(EXTRA_mtime);
 
         dImageView.setImageBitmap(iImageResource);
         dCountryName.setText(iCountryName);
@@ -95,7 +93,21 @@ public class DetailActivity extends AppCompatActivity {
         dDateTime.setText(iDateTime);
     }
 
-    // function to store flag images insde an array
-    
+    @Override
+    protected void onPause() {
+        Log.d("detail","onPause");
+        super.onPause();
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(6*60*1000);
+                    // Wipe your valuable data here
+                    finishAffinity();
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }.start();
+    }
 }
 
